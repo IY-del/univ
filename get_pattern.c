bed4595736859131fbb2462e5976e9fee6bea7d1
ishp_ctrin <s26G1012DS@chibatech.ac.jp>
2026-07-13 10:15:37 +0900

    Add load_txt function for loading binary data and update header

--- get_pattern.c
+++ get_pattern.c
@@ -3,7 +3,7 @@
 #include <ctype.h>
 #include "get_pattern.h"
 
-int load_txt_to_padded_8x8(const char filename[], int (**out)[8][8], size_t *out_n) {
+int load_txt_to_8x8(const char filename[], int (**out)[8][8], size_t *out_n) {
     FILE *fp = fopen(filename, "r");
     if (!fp) {
         return -1;
@@ -54,6 +54,41 @@ int load_txt_to_padded_8x8(const char filename[], int (**out)[8][8], size_t *out
     return 0;
 }
 
-void free_padded_8x8(int (*arr)[8][8]) {
+int load_txt(const char *filename, size_t h, size_t w, int **out, size_t *out_n) {
+    FILE *fp = fopen(filename, "r");
+    if (!fp) return -1;
+
+    size_t cap = 1024, len = 0;
+    int *buf = malloc(cap * sizeof(int));
+    if (!buf) { fclose(fp); return -2; }
+
+    int ch;
+    while ((ch = fgetc(fp)) != EOF) {
+        if (ch == '0' || ch == '1') {
+            if (len == cap) {
+                cap *= 2;
+                int *tmp = realloc(buf, cap * sizeof(int));
+                if (!tmp) { free(buf); fclose(fp); return -2; }
+                buf = tmp;
+            }
+            buf[len++] = ch - '0';
+        }
+    }
+    fclose(fp);
+
+    size_t cell = h * w;
+    size_t n = (len + cell - 1) / cell;
+    int *arr = calloc(n * cell, sizeof(int));
+    if (!arr) { free(buf); return -2; }
+
+    for (size_t i = 0; i < len; i++) arr[i] = buf[i];
+    free(buf);
+
+    *out = arr;
+    *out_n = n;
+    return 0;
+}
+
+void free_memory(int *arr) {
     free(arr);
 }
