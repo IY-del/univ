205de19505a064b41936d1aa8a69d7dd33ba6e78
ishp_ctrin <s26G1012DS@chibatech.ac.jp>
2026-07-26 17:24:35 +0900

    Refactor coordinate handling in get_coord and iterator modules for improved readability and maintainability

--- get_coord.c
+++ get_coord.c
@@ -1,54 +1,51 @@
 #include "get_coord.h"
 
-static PatternCoord rotate_coord(const PatternView *view, size_t x, size_t y) {
+static PatternCoord rotate_coord(const PatternView *view, size_t x, size_t y)
+{
     PatternCoord coord = {0, 0};
-    switch (view->rotation) {
-        case ROTATE_0:
-            coord.x = x;
-            coord.y = y;
-            break;
-        case ROTATE_90:
-            coord.x = y;
-            coord.y = view->h - 1 - x;
-            break;
-        case ROTATE_180:
-            coord.x = view->w - 1 - x;
-            coord.y = view->h - 1 - y;
-            break;
-        case ROTATE_270:
-            coord.x = view->w - 1 - y;
-            coord.y = x;
-            break;
+    switch (view->rotation)
+    {
+    case ROTATE_0:
+        coord.x = x;
+        coord.y = y;
+        break;
+    case ROTATE_90:
+        coord.x = y;
+        coord.y = view->h - 1 - x;
+        break;
+    case ROTATE_180:
+        coord.x = view->w - 1 - x;
+        coord.y = view->h - 1 - y;
+        break;
+    case ROTATE_270:
+        coord.x = view->w - 1 - y;
+        coord.y = x;
+        break;
     }
 
     return coord;
 }
 
-size_t pattern_output_width(const PatternView *view) {
-    if (view->rotation == ROTATE_90 || view->rotation == ROTATE_270) {
-        return view->h;
+PatternSize pattern_scaled_size(const PatternView *view)
+{
+    PatternSize size;
+
+    if (view->rotation == ROTATE_90 || view->rotation == ROTATE_270)
+    {
+        size.width = view->h * view->scale_w;
+        size.height = view->w * view->scale_h;
+    }
+    else
+    {
+        size.width = view->w * view->scale_w;
+        size.height = view->h * view->scale_h;
     }
 
-    return view->w;
+    return size;
 }
 
-size_t pattern_output_height(const PatternView *view) {
-    if (view->rotation == ROTATE_90 || view->rotation == ROTATE_270) {
-        return view->w;
-    }
-    
-    return view->h;
-}
-
-size_t pattern_scaled_width(const PatternView *view) {
-    return pattern_output_width(view) * view->scale_w;
-}
-
-size_t pattern_scaled_height(const PatternView *view) {
-    return pattern_output_height(view) * view->scale_h;
-}
-
-PatternCoord resolve_coord(const PatternView *view, size_t out_x, size_t out_y) {
+PatternCoord resolve_coord(const PatternView *view, size_t out_x, size_t out_y)
+{
     size_t logical_x = out_x / view->scale_w;
     size_t logical_y = out_y / view->scale_h;
     return rotate_coord(view, logical_x, logical_y);
--- get_coord.h
+++ get_coord.h
@@ -4,16 +4,19 @@
 #include <stddef.h>
 #include "print_pattern.h"
 
-typedef struct {
+typedef struct
+{
     size_t x;
     size_t y;
 } PatternCoord;
 
-size_t pattern_output_width(const PatternView *view);
-size_t pattern_output_height(const PatternView *view);
-size_t pattern_scaled_width(const PatternView *view);
-size_t pattern_scaled_height(const PatternView *view);
+typedef struct
+{
+    size_t width;
+    size_t height;
+} PatternSize;
 
+PatternSize pattern_scaled_size(const PatternView *view);
 PatternCoord resolve_coord(const PatternView *view, size_t out_x, size_t out_y);
 
 #endif
