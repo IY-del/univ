2cad38e3e780eb13b80cd87ac00eb56dc43e77e7
ishp_ctrin <s26G1012DS@chibatech.ac.jp>
2026-07-26 19:36:23 +0900

    Refactor test functions to improve structure and error messages; update test script for clarity

--- test/test.c
+++ test/test.c
@@ -6,8 +6,9 @@
 #include "iterator.h"
 #include "print_pattern.h"
 
-/* fontlib テスト */
-static void test_fontlib(void) {
+/* fontlib のテスト ------------------------------------------------------- */
+#ifdef TEST_FONTLIB
+void test_fontlib(void) {
   printx(1);
   printx(0);
   printx(2);
@@ -19,14 +20,16 @@ static void test_fontlib(void) {
   printxs(0, "ABC");
   putchar('\n');
 }
+#endif
 
-/* get_pattern テスト */
+/* get_pattern のテスト --------------------------------------------------- */
+#ifdef TEST_GET_PATTERN
 void test_get_pattern(void) {
   int (*patterns)[8][8];
   size_t n;
 
   if (load_txt_to_8x8("../font_data01.txt", &patterns, &n) != 0) {
-    fprintf(stderr, "load_txt_to_8x8 failed\n");
+    fprintf(stderr, "load_txt_to_8x8 failed (../font_data01.txt)\n");
     return;
   }
 
@@ -43,10 +46,13 @@ void test_get_pattern(void) {
 
   free_memory(patterns);
 }
+#endif
 
-/* get_coord テスト */
-static void test_get_coord(void) {
+/* get_coord のテスト ----------------------------------------------------- */
+#ifdef TEST_GET_COORD
+void test_get_coord(void) {
   int dummy[1][8][8] = {{{0}}};
+
   PatternView view = new_pattern_view(1, dummy);
 
   PatternSize size1 = pattern_scaled_size(&view);
@@ -61,9 +67,19 @@ static void test_get_coord(void) {
 
   PatternCoord src2 = resolve_coord(&view, 0, 0);
   printf("rotate90 resolve(0,0) -> (%zu,%zu)\n", src2.x, src2.y);
-}
 
-/* iterator テスト */
+  view.scale_w = 2;
+  view.scale_h = 2;
+  PatternSize size3 = pattern_scaled_size(&view);
+  printf("rotate90 scale2: width=%zu height=%zu\n", size3.width, size3.height);
+
+  PatternCoord src3 = resolve_coord(&view, 1, 1);
+  printf("rotate90 scale2 resolve(1,1) -> (%zu,%zu)\n", src3.x, src3.y);
+}
+#endif
+
+/* iterator のテスト ------------------------------------------------------ */
+#ifdef TEST_ITERATOR
 static void run_next(const PatternView* view) {
   PatternIterator it = new_iter(view);
   PatternStep step;
@@ -94,16 +110,19 @@ static void run_next_h(const PatternView* view) {
   printf("END\n");
 }
 
-static void test_iterator(void) {
+void test_iterator(void) {
   int dummy[2][8][8] = {{{0}}};
   PatternView view = new_pattern_view(2, dummy);
+
   run_next(&view);
   putchar('\n');
   run_next_h(&view);
 }
+#endif
 
-/* print_pattern テスト */
-static void test_print_pattern(void) {
+/* print_pattern のテスト ------------------------------------------------- */
+#ifdef TEST_PRINT_PATTERN
+void test_print_pattern(void) {
   int patterns[1][8][8] = {{{0, 0, 1, 1, 0, 0, 0, 0},
                             {0, 0, 1, 1, 0, 0, 0, 0},
                             {0, 0, 1, 1, 0, 0, 0, 0},
@@ -114,9 +133,36 @@ static void test_print_pattern(void) {
                             {0, 0, 0, 0, 0, 0, 0, 0}}};
 
   PatternView view = new_pattern_view(1, patterns);
+  printf("normal: h=%zu w=%zu scale_w=%zu scale_h=%zu rotation=%d\n", view.h,
+         view.w, view.scale_w, view.scale_h, view.rotation);
   print_as_pattern(&view);
-}
 
+  PatternView view2x = new_pattern_view_doublescale(1, patterns);
+  printf("double: h=%zu w=%zu scale_w=%zu scale_h=%zu rotation=%d\n", view2x.h,
+         view2x.w, view2x.scale_w, view2x.scale_h, view2x.rotation);
+  print_as_pattern(&view2x);
+}
+#endif
+
+/* main 相当のテスト ------------------------------------------------------ */
+#ifdef TEST_MAIN_LIKE
+void test_main_like(void) {
+  int (*patterns)[8][8];
+  size_t n;
+
+  if (load_txt_to_8x8("../font_data01.txt", &patterns, &n) != 0) {
+    fprintf(stderr, "failed to load ../font_data01.txt\n");
+    return;
+  }
+
+  PatternView view = new_pattern_view(n, patterns);
+  print_as_pattern(&view);
+
+  free_memory(patterns);
+}
+#endif
+
+/* エントリポイント ------------------------------------------------------- */
 int main(void) {
 #ifdef TEST_FONTLIB
   test_fontlib();
@@ -128,6 +174,8 @@ int main(void) {
   test_iterator();
 #elif defined(TEST_PRINT_PATTERN)
   test_print_pattern();
+#elif defined(TEST_MAIN_LIKE)
+  test_main_like();
 #else
   printf("No test macro defined.\n");
 #endif
